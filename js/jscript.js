9c46ce2bb7c5935d6249c6dc3866cd221f696802
Nirmal-Ram <nirmalramofficial@gmail.com>
2020-12-26 19:09:09 +0530

    jscript updated

--- js/jscript.js
+++ js/jscript.js
@@ -1,1 +1,50 @@
-var a=10;
+$(function(){
+    $('#register').click(function(e){
+
+        var valid = this.form.checkValidity();
+
+        if(valid){
+
+
+        var username = $('#username').val();
+        var emailid	= $('#emailid').val();
+        var passwords = $('#passwords').val();
+        var cpassword = $('#cpassword').val();
+
+        if(passwords==cpassword){
+        }
+        else{
+            return;
+        }
+
+            e.preventDefault();	
+
+            $.ajax({
+                type: 'POST',
+                url: 'user.php',
+                data: {username: username,emailid: emailid,password: password},
+                success: function(data){
+                Swal.fire({
+                            'title': 'Successful',
+                            'text': data,
+                            'type': 'success'
+                            })
+                        
+                },
+                error: function(data){
+                    Swal.fire({
+                            'title': 'Errors',
+                            'text': 'There were errors while saving the data.',
+                            'type': 'error'
+                            })
+                }
+            });
+
+            
+        }else{
+            
+        }
+    });		
+
+    
+});
--- signup.php
+++ signup.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require 'html/signup.html';
+$_SESSION['username']=
 
 
 ?>
