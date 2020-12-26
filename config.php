0fbe35dda257270741cb353c7ce9183e34d7ecbb
Nirmal-Ram <nirmalramofficial@gmail.com>
2020-12-26 17:38:24 +0530

    database management

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Created!
+// You have successfully created a new database. The details are below.
+// Username: QExKt8jTh3
+// Database name: QExKt8jTh3
+// Password: XqrylbkApz
+// Server: remotemysql.com
+// Port: 3306
+// These are the username and password to log in to your database and phpMyAdmin
+
+$servername = "remotemysql.com";
+$database = "QExKt8jTh3";
+$username = "QExKt8jTh3";
+$password = "XqrylbkApz";
+
+$db = new PDO('mysql:host='.$servername.';dbname=' . $database . ';charset=utf8', $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
