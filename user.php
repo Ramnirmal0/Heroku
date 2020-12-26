f636a0a5dde57cc312267913be28febbe622dc8f
Nirmal-Ram <nirmalramofficial@gmail.com>
2020-12-26 22:11:27 +0530

    signup logic rectified

--- user.php
+++ user.php
@@ -1,16 +1,14 @@
 <?php
 session_start();
+$username 		= $_SESSION['username'];
+$emailid 		= $_SESSION['emailid'];
+$password 		= $_SESSION['password'];
 require_once('config.php');
 require 'html/user.html';
-?>
-
-<?php 
 
 if(isset($_POST['continue'])){
 
-	$username 		= $_SESSION['username'];
-	$emailid 		= $_SESSION['emailid'];
-	$password 		= $_SESSION['password'];
+
 	$dob = $_POST['dob'];
 	$location=$_POST['location'];
 	$mob=$_POST['mob'];
