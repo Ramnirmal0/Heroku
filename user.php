43852dd38417fb0c869784f20aca75e8c78959f0
Nirmal-Ram <nirmalramofficial@gmail.com>
2020-12-27 12:32:36 +0530

    upadted php user file

--- user.php
+++ user.php
@@ -2,17 +2,19 @@
 session_start();
 require_once('config.php');
 require 'html/user.html';
+?>
 
+<?php
 
 if(isset($_POST)){
 
-		$username 		= $_POST['username'];
-		$emailid 			= $_POST['emailid'];
-		$passwords 		= sha1($_POST['passwords']);
+		$username= $_POST['username'];
+		$emailid = $_POST['emailid'];
+		$passwords = sha1($_POST['passwords']);
 		$_SESSION['username']=$username;
 		$_SESSION['emailid']=$emailid;
 		$_SESSION['passwords']=$passwords;
-		$sql = "INSERT INTO accounts (username, email, password ) VALUES(?,?,?)";
+		$sql = "INSERT INTO accounts (username, email, passwords ) VALUES(?,?,?)";
 		$stmtinsert = $db->prepare($sql);
 		$result = $stmtinsert->execute([$username, $emailid, $passwords]);
 		if($result){
