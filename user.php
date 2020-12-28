e25609069530d95dea72e2d973261023e9326ab8
Nirmal-Ram <nirmalramofficial@gmail.com>
2020-12-28 15:06:08 +0530

    db update

--- user.php
+++ user.php
@@ -19,24 +19,52 @@ if(isset($_POST)){
 				$dbhost = 'sql104.epizy.com';
 				$dbuser = 'epiz_27561235';
 				$dbpass = 'project@1234';
-				$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+				$database = 'accounts';
+
+				$conn = new mysqli($dbhost, $dbuser, $dbpass, $database);
+
+			// Check connection
+			if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+			}
+
+			//$sql = "INSERT INTO accounts (username, email, passwords) VALUES (:uname, :email, :pwd)";
+
+			$stmt = $conn->prepare("INSERT INTO accounts (uid , username, email, passwords , name , dob , location , mob) VALUES (?, ?, ?,?,?,?,?,?)");
+			$stmt->bind_param("sss", " ",$username, $email, $password," "," "," "," ");
+
+			$username = $uname;
+			$email = $email;
+			$password = $pwd;
+			$stmt->execute();
+
+		     if($stmt){
+				 
+				 echo "$username - $email - $password ";
+			 }
+
+			$stmt->close();
+			$conn->close(); 
+
+
+				// $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 			 
-				if(! $conn ) {
-				   die('Could not connect: ' . mysqli_error());
-				}
+				// if(! $conn ) {
+				//    die('Could not connect: ' . mysqli_error());
+				// }
 	   
-				$sql = "INSERT INTO accounts ".
-				   "(username,email, passwords) "."VALUES ".
-				   "('$uname','$email','$pwd')";
-				   mysqli_select_db('accounts');
-				$retval = mysqli_query( $sql, $conn );
+				// $sql = "INSERT INTO accounts ".
+				//    "(username,email, passwords) "."VALUES ".
+				//    "('$uname','$email','$pwd')";
+				//    mysqli_select_db('accounts');
+				// $retval = mysqli_query( $sql, $conn );
 			 
-				if(! $retval ) {
-				   die('Could not enter data: ' . mysqli_error());
-				}
+				// if(! $retval ) {
+				//    die('Could not enter data: ' . mysqli_error());
+				// }
 			 
-				echo "Entered data successfully\n";
-				mysqli_close($conn);
+				// echo "Entered data successfully\n";
+				// mysqli_close($conn);
 
 }
 
