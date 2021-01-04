9385615f80ee00d331d3f74ca814fcee756fa4e3
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-04 20:32:05 +0530

    updated error page

--- error404.php
+++ error404.php
@@ -27,8 +27,8 @@
         <div class="container-fluid">
         <center>
             <img src = "../img/build.gif" ><br><br>
-            <h3> Error 404 ! Page Not Found Exception </h3><br>
-            <h4>This page is under construction. please click back button to go back</h4>
+            <h1> Error 404 ! Page Not Found Exception </h3><br>
+            <h3>This page is currently under construction. please click back button to go back</h4>
         </center>
         </div>   
 
