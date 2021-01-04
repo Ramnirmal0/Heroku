0d79cede024872367c60740d2ffaf752328dcb57
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-04 14:37:11 +0530

    sqli update

--- process.php
+++ process.php
@@ -5,26 +5,61 @@
         $uname= $_POST['userid'];
         $pwd = sha1($_POST['pwd']);
 
-        $mysqli = new mysqli("remotemysql.com","QExKt8jTh3","XqrylbkApz","QExKt8jTh3");
-
-        if ($mysqli -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-        exit();
+        $conn = mysqli_connect("remotemysql.com","QExKt8jTh3","XqrylbkApz","QExKt8jTh3") ;
+ 
+        if (!$conn)
+        {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
 
-        // Turn autocommit off
-        $mysqli -> autocommit(FALSE);
+        $stmt = $conn->prepare("SELECT email, password FROM register_users where email='$uname' and password = '$pwd'");
+        
+            /* execute statement */
+            $stmt->execute();
+        
+            /* bind result variables */
+            $stmt->bind_result($user, $pass);
+        
+            /* fetch values */
+            while ($stmt->fetch()) 
+            {
+                
+            }
+        
+        if($user == $username)
+                {
+                $_SESSION['name'] = $user;
+                // Redirect user to index.php
+                header("Location: account.php");
+                }
+                else
+                {
+                    echo "Your username or password is wrong";
+                }
+        
+        
+        $stmt->close();
 
-        $result = $mysqli -> query("SELECT * FROM Persons WHERE email='user@mail.com' and password='5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8
-        '");
-        $output=$result -> num_rows;
-        if($output==1){
-            header('Location: ../account.php');
-        }
-        else{
-            echo "error finding . output is $result ";
-        }
+        // $mysqli = new mysqli("remotemysql.com","QExKt8jTh3","XqrylbkApz","QExKt8jTh3");
 
-        $mysqli -> close(); 
+        // if ($mysqli -> connect_errno) {
+        // echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        // exit();
+        // }
+
+        // // Turn autocommit off
+        // $mysqli -> autocommit(FALSE);
+
+        // $result = $mysqli -> query("SELECT * FROM Persons WHERE email='user@mail.com' and password='5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8
+        // '");
+        // $output=$result -> num_rows;
+        // if($output==1){
+        //     header('Location: ../account.php');
+        // }
+        // else{
+        //     echo "error finding . output is $result ";
+        // }
+
+        // $mysqli -> close(); 
     }
 ?>
